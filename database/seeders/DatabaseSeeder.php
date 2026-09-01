92c961ed831e55485feb99ac6a2a8baaefb35dcc
reijack <thealanjack@gmail.com>
2026-09-01 18:17:50 +0700

    Refactor DatabaseSeeder to remove test user creation
    Removed user factory creation for test users and retained admin user creation.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         User::updateOrCreate(
             ['email' => 'admin@travel.com'],
             [
